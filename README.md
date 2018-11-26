04f6a64c73bb3f84db51743635071b273d809588
Sharif H. Khan <sharifhkhan@me.com>
2018-11-26 02:42:27 -0600

    start of blackjack

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 Javascript playground
 
 basics.js - some JS nuances worth noting
+blackjack.js - a simple blackjack game 
---
+++ blackjack/blackjack.js
@@ -0,0 +1,46 @@
+const SUITS = ['Spades', 'Hearts', 'Clubs', 'Diamonds']
+const CARDS = ['Ace', 'King', 'Queen', 'Jack', '10', '9', '8', '7', '6', '5', '4', '3', '2']
+
+let dealerCards = []
+let playerCards = []
+let deck = createDeck()
+
+function createDeck() {
+    let deck = []
+    SUITS.forEach(s => {
+        CARDS.forEach(c => {
+
+            let card = {
+                suit: s,
+                value: c
+            }
+
+            deck.push(card)
+        })
+    })
+
+    return deck;
+}
+
+function getCardString(card) {
+    return card.value + ' of ' + card.suit
+}
+
+function randomIntFromInterval(min, max) // min and max included
+{
+    //    return Math.floor(Math.random()*(max-min+1)+min)
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+function getStartingCards() {
+    let card1 = getCardString(deck[randomIntFromInterval(50, 51)])
+    let card2 = getCardString(deck[randomIntFromInterval(50, 51)])
+    while (card1 === card2) {
+        card2 = getCardString(deck[randomIntFromInterval(50, 51)])
+    }
+    console.log(card1)
+    console.log(card2)
+}
+
+getStartingCards()
